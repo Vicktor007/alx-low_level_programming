a345147b3d121e25bc7f1c17994ec70ce83df8c7
Vicktor007 <vicktord007@gmail.com>
2022-09-09 14:49:32 -0700

    2print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,26 +2,14 @@
 /**
  * main - entry point
  *
- * Return: alway returns 0*
+ * Return: alway returns 0
  *
  */
 int main(void)
 {
-int i;
-int j;
-for (i = 10; i <= 19; i++)
-{
-for (j = 10; j <= 19; j++)
-{
-putchar((i % 10) + '0');
-putchar((j % 10) + '0');
-if (i != 19 ||  j != 19)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
+char a;
+for (a = 'a'; a <= 'z' ; a++)
+putchar(a);
 putchar('\n');
 return (0);
 }
