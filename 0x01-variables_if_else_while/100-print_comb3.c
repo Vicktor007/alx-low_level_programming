780f9c7fa48283a008d9409c0a22c87f51a9c4cd
Vicktor007 <vicktord007@gmail.com>
2022-09-09 15:56:13 -0700

    100print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,19 @@ int main(void)
 {
 int i;
 int j;
-for (i = 10; i <= 19; i++)
+for (i = 48; i < 58; i++)
 {
-for (j = 10; j <= 19; j++)
+for (j = 48; j < 58; j++)
 {
-putchar((i % 10) + '0');
-putchar((j % 10) + '0');
-if (i != 19 ||  j != 19)
+if (i != j && i < j)
 {
+putchar(i);
+putchar(j);
+if (j == 57 && i == 56)
+{
+break
+;
+}
 putchar(',');
 putchar(' ');
 }
