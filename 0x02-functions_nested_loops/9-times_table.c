0f8f7a884bcd0b1f69e11ce97c666706465c74f8
Vicktor007 <vicktord007@gmail.com>
2022-09-13 10:47:48 -0700

    times tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,40 +6,32 @@
  */
 void times_table(void)
 {
-int a;
-int b;
-int c;
-for (a = 0; a <= 9; a++)
+int i;
+int j;
+for (i = 0; i <= 9; i++)
 {
-c = a * b;
-if ((c / 10) == 0)
+for (j = 0; j <= 9; j++)
 {
-if (b == 0)
+int prod = j * i;
+if (j == 0)
 {
-_putchar ('0');
+_putchar('0');
 }
-if (b != 0)
-{
-_putchar(' ');
-_putchar((c % 10) + '0');
-}
-if (b < 9)
+else if (prod <= 9)
 {
 _putchar(',');
 _putchar(' ');
-}
+_putchar(' ');
+_putchar(prod + '0');
 }
 else
 {
-_putchar ((c / 10) + '0');
-_putchar ((c % 10) + '0');
-if (b < 9)
-{
 _putchar(',');
 _putchar(' ');
+_putchar(prod / 10 + '0');
+_putchar(prod % 10 + '0');
 }
 }
+_putchar('\n');
 }
-_putchar ('\n');
 }
-
