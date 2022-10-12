f82240bc2bdf8d94d0a78c13498ad4e7735458c9
Vicktor007 <vicktord007@gmail.com>
2022-10-12 10:40:37 -0700

    first

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "function_pointers.h"
 /**
  * print_name - prints a name
@@ -7,7 +8,6 @@
  */
 void print_name(char name, void (*f)(char *))
 {
-if (!name || !f);
-return;
+if (name != NULL && f != NULL)
 f(name);
 }
