aa7e84e8f501a305d4f2769a46aada6251706c2c
Vicktor007 <vicktord007@gmail.com>
2022-10-20 13:59:44 -0700

    done

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,12 +8,11 @@
 
 void free_list(list_t *head)
 {
-    list_t *current;
-
-    while ((current = head) != NULL)
-    {
-        head = head->next;
-        free(current->str);
-        free(current);
-    }
+list_t *current;
+while ((current = head) != NULL)
+{
+head = head->next;
+free(current->str);
+free(current);
+}
 }
